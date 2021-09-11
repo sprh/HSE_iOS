dcfd13b44be75c26a7ff1c0a31ca67cd5c206fd3
sktimokhina <sktimokhina@yandex-team.ru>
2021-09-11 08:40:37 +0300

    Added an app icon, several colors (background, two modes for a button, text color), extensions for UIColor and UIFont

---
+++ sktimokhina@edu.hse.ruPW1/sktimokhina@edu.hse.ruPW1/Utils/UIColor+Utils.swift
@@ -0,0 +1,27 @@
+//
+//  UIColor+Utils.swift
+//  sktimokhina@edu.hse.ruPW1
+//
+//  Created by Софья Тимохина on 11.09.2021.
+//
+
+import UIKit
+
+extension UIColor {
+    // Static vars (colors from assets).
+    static var background: UIColor {
+        UIColor(named: "background")!
+    }
+
+    static var text: UIColor {
+        UIColor(named: "text")!
+    }
+
+    static var button: UIColor {
+        UIColor(named: "button")!
+    }
+
+    static var disabledButton: UIColor {
+        UIColor(named: "disabledButton")!
+    }
+}
---
+++ sktimokhina@edu.hse.ruPW1/sktimokhina@edu.hse.ruPW1/Utils/UIFont+Utils.swift
@@ -0,0 +1,15 @@
+//
+//  UIFont+Utils.swift
+//  sktimokhina@edu.hse.ruPW1
+//
+//  Created by Софья Тимохина on 11.09.2021.
+//
+
+import UIKit
+
+extension UIFont {
+    static var buttonFont: UIFont {
+        .systemFont(ofSize: 20)
+    }
+}
+
