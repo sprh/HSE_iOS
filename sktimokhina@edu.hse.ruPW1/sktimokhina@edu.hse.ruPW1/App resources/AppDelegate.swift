e3e9b35f7179c129e7bbf9c80cbfeefd2274b11a
sktimokhina <sktimokhina@yandex-team.ru>
2021-09-11 08:36:16 +0300

    Added the project structure

---
+++ sktimokhina@edu.hse.ruPW1/sktimokhina@edu.hse.ruPW1/App resources/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  sktimokhina@edu.hse.ruPW1
+//
+//  Created by Софья Тимохина on 11.09.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
