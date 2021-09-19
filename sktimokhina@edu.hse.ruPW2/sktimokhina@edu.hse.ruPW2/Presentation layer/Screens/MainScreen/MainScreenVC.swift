8cd655d53d69f71330b917c3a4af1852c9effb0c
sktimokhina <sktimokhina@yandex-team.ru>
2021-09-19 12:50:12 +0300

    The main screen ui setupping

--- sktimokhina@edu.hse.ruPW2/sktimokhina@edu.hse.ruPW2/Presentation layer/Screens/MainScreen/MainScreenVC.swift
+++ sktimokhina@edu.hse.ruPW2/sktimokhina@edu.hse.ruPW2/Presentation layer/Screens/MainScreen/MainScreenVC.swift
@@ -18,6 +18,30 @@ protocol IMainScreenVC: UIViewController {
 final class MainScreenVC: UIViewController {
     var interactor: IMainScreenInteractor!
     private var router: IMainScreenRouter
+
+    private var locationStack: UIStackView = {
+        let stack = UIStackView()
+        stack.backgroundColor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.layer.cornerRadius = 20
+        return stack
+    }()
+
+    private var location: UILabel = {
+        let location = UILabel()
+        location.translatesAutoresizingMaskIntoConstraints = false
+        location.font = UIFont.preferredFont(forTextStyle: .title1)
+        return location
+    }()
+
+    private var locationLabel: UILabel = {
+        let location = UILabel()
+        location.translatesAutoresizingMaskIntoConstraints = false
+        location.textAlignment = .center
+        location.font = UIFont.preferredFont(forTextStyle: .title1)
+        location.text = "Your location is.."
+        return location
+    }()
     
     init(router: IMainScreenRouter) {
         self.router = router
@@ -32,6 +56,7 @@ final class MainScreenVC: UIViewController {
         super.viewDidLoad()
         interactor.shouldUpdateView()
         setupNavigationController()
+        setupUI()
     }
 
     private func setupNavigationController() {
@@ -39,6 +64,23 @@ final class MainScreenVC: UIViewController {
         navigationItem.title = "Main"
         let settingsButton = UIBarButtonItem(image: UIImage(systemName: "gearshape.fill"), style: .plain, target: self, action: #selector(didTapSettingsButton))
         navigationItem.rightBarButtonItem = settingsButton
+    }
+
+    private func setupUI() {
+        view.addSubview(locationStack)
+        locationStack.addSubview(location)
+        locationStack.addSubview(locationLabel)
+        NSLayoutConstraint.activate([
+            locationLabel.leadingAnchor.constraint(equalTo: locationStack.leadingAnchor, constant: 16),
+            locationLabel.trailingAnchor.constraint(equalTo: locationStack.trailingAnchor, constant: -16),
+            locationLabel.topAnchor.constraint(equalTo: locationStack.topAnchor, constant: 16),
+            location.bottomAnchor.constraint(equalTo: locationLabel.bottomAnchor, constant: 16),
+            location.centerXAnchor.constraint(equalTo: locationLabel.centerXAnchor),
+            location.bottomAnchor.constraint(equalTo: locationStack.bottomAnchor, constant: -16),
+
+            locationStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            locationStack.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 
     @objc func didTapSettingsButton() {
