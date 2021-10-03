47c1fdabf6a3dfa38a55016ef67204e731dd4566
sktimokhina <sktimokhina@yandex-team.ru>
2021-10-03 22:52:21 +0300

    Added icons on the tabbar

--- sktimokhina@edu.hse.ruPW3/sktimokhina@edu.hse.ruPW3/PresentationLayer/CustomElements/TabBarController.swift
+++ sktimokhina@edu.hse.ruPW3/sktimokhina@edu.hse.ruPW3/PresentationLayer/CustomElements/TabBarController.swift
@@ -11,18 +11,24 @@ final class TabBarController: UITabBarController {
     lazy var stackVC: UIViewController = {
         let stackVC = StackGraph().viewController
         stackVC.tabBarItem.title = "Stack"
+        stackVC.tabBarItem.image = UIImage(systemName: "i.circle")
+        stackVC.tabBarItem.selectedImage = UIImage(systemName: "i.circle.fill")
         return stackVC
     }()
 
     lazy var collectionVC: UIViewController = {
         let collectionVC = CollectionGraph().viewController
         collectionVC.tabBarItem.title = "Collection"
+        collectionVC.tabBarItem.image = UIImage(systemName: "h.circle")
+        collectionVC.tabBarItem.selectedImage = UIImage(systemName: "h.circle.fill")
         return collectionVC
     }()
 
     lazy var tableVC: UIViewController = {
         let tableVC = TableGraph().viewController
         tableVC.tabBarItem.title = "Table"
+        tableVC.tabBarItem.image = UIImage(systemName: "exclamationmark")
+        tableVC.tabBarItem.selectedImage = UIImage(systemName: "exclamationmark.3")
         return tableVC
     }()
 
