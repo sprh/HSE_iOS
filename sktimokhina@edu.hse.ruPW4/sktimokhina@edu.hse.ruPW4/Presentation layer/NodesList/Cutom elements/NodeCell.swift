5af21628bb6795b8b97e7536e4e3a18df53275ec
sprh <sprh.tt@gmail.com>
2021-10-19 16:21:37 +0300

    Added actions, updated cell

--- sktimokhina@edu.hse.ruPW4/sktimokhina@edu.hse.ruPW4/Presentation layer/NodesList/Cutom elements/NodeCell.swift
+++ sktimokhina@edu.hse.ruPW4/sktimokhina@edu.hse.ruPW4/Presentation layer/NodesList/Cutom elements/NodeCell.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 final class NodeCell: UICollectionViewCell {
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        backgroundColor = .red
-    }
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .title1)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Title"
+        return label
+    }()
 
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.text = "Description"
+        return label
+    }()
+
+    func setup() {
+        backgroundColor = #colorLiteral(red: 1, green: 0.8220604658, blue: 0.8168862462, alpha: 1)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(descriptionLabel)
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
+
+            descriptionLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            descriptionLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16),
+            descriptionLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
+        ])
     }
 }
--- sktimokhina@edu.hse.ruPW4/sktimokhina@edu.hse.ruPW4/Presentation layer/NodesList/NodesListVC.swift
+++ sktimokhina@edu.hse.ruPW4/sktimokhina@edu.hse.ruPW4/Presentation layer/NodesList/NodesListVC.swift
@@ -14,15 +14,30 @@ final class NodesListVC: UIViewController, INodesListVC {
     private let interactor: INodesListInteractor
     private let router: INodesListRouter
 
+    lazy var collectionViewListConfiguration: UICollectionLayoutListConfiguration = {
+        var listConfig = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        listConfig.trailingSwipeActionsConfigurationProvider = { [weak self] indexPath in
+            guard let self = self else { return nil }
+            let actionHandler: UIContextualAction.Handler = { action, view, completion in
+                completion(true)
+            }
+            let action = UIContextualAction(style: .normal, title: "Delete", handler: actionHandler)
+            action.image = UIImage(systemName: "trash")
+            action.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+            return UISwipeActionsConfiguration(actions: [action])
+        }
+        listConfig.separatorConfiguration.color = .white
+        return listConfig
+    }()
+
     lazy var collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.estimatedItemSize = CGSize(width: 100, height: 100)
+        let layout = UICollectionViewCompositionalLayout.list(using: collectionViewListConfiguration)
         let collectionView = UICollectionView(frame: view.frame, collectionViewLayout: layout)
         collectionView.delegate = self
         collectionView.dataSource = self
         collectionView.backgroundColor = view.backgroundColor
-        collectionView.contentInset = UIEdgeInsets(top: 0, left: 16, bottom: view.safeAreaInsets.bottom, right: 16)
         collectionView.alwaysBounceVertical = true
+        collectionView.contentInsetAdjustmentBehavior = .always
         collectionView.register(NodeCell.self, forCellWithReuseIdentifier: "\(NodeCell.self)")
         return collectionView
     }()
@@ -60,6 +75,29 @@ extension NodesListVC: UICollectionViewDelegate, UICollectionViewDataSource {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(NodeCell.self)", for: indexPath) as? NodeCell else {
             return UICollectionViewCell()
         }
+        cell.setup()
         return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, contextMenuConfigurationForItemAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
+//        guard let cell = collectionView.cellForItem(at: indexPath) as? NodeCell else {
+//            return nil
+//        }
+        let context = UIContextMenuConfiguration(identifier: nil, previewProvider: nil) { (action) -> UIMenu? in
+
+            let delete = UIAction(title: "Delete", image: UIImage(systemName: "trash"), identifier: nil, discoverabilityTitle: nil,attributes: .destructive, state: .off) { (_) in
+            }
+
+            let edit = UIAction(title: "Edit", image: UIImage(systemName: "square.and.pencil"), identifier: nil, discoverabilityTitle: nil, state: .off) { (_) in
+            }
+
+            return UIMenu(title: "Options", image: nil, identifier: nil, options: UIMenu.Options.displayInline, children: [edit,delete])
+
+        }
+        return context
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
     }
 }
