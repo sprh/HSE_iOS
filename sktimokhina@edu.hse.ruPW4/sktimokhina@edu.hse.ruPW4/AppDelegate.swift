490f0b1359bcf1ef8990874c92d6ca192b50a8f2
sprh <sprh.tt@gmail.com>
2021-10-22 00:47:38 +0300

    fixed relation

--- sktimokhina@edu.hse.ruPW4/sktimokhina@edu.hse.ruPW4/AppDelegate.swift
+++ sktimokhina@edu.hse.ruPW4/sktimokhina@edu.hse.ruPW4/AppDelegate.swift
@@ -23,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "sktimokhina_edu_hse_ruPW4")
+        let container = NSPersistentContainer(name: "Notes_first_version")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
