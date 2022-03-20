d8d6984f50ad0de040f14451ce43ea3926715aca
sprh <sprh.tt@gmail.com>
2022-03-20 12:21:05 +0300

    Added new project

---
+++ sktimokhinaPW8/sktimokhinaPW8/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  sktimokhinaPW8
+//
+//  Created by Софья Тимохина on 20.03.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
